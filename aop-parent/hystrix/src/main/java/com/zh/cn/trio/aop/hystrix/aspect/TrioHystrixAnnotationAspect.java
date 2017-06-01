630ec0d035b1d3d1a41c899b70cb05366733b1ba
Administrator <Administrator@LP-JAVA-WZL>
2017-06-01 16:07:30 +0800

    add scm for maven deploy

--- aop-parent/hystrix/src/main/java/com/zh/cn/trio/aop/hystrix/aspect/TrioHystrixAnnotationAspect.java
+++ aop-parent/hystrix/src/main/java/com/zh/cn/trio/aop/hystrix/aspect/TrioHystrixAnnotationAspect.java
@@ -1,44 +1,44 @@
-package com.zh.cn.trio.aop.hystrix.aspect;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-
-import com.zh.cn.trio.aop.hystrix.annotation.TrioHystrix;
-
-import io.github.zh.cn.trio.aop.croe.aspect.annotation.AbstractAnnotationConfigAspect;
-import io.github.zh.cn.trio.aop.croe.base.format.Format;
-import io.github.zh.cn.trio.aop.croe.strategy.AopStrategy;
-import io.github.zh.cn.trio.aop.validation.config.ValidationBeanConfig;
-
-@Aspect
-public class TrioHystrixAnnotationAspect extends AbstractAnnotationConfigAspect<ValidationBeanConfig, TrioHystrix> {
-
-	@Override
-	public Class<TrioHystrix> getAnnotationClass() {
-		return TrioHystrix.class;
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public ValidationBeanConfig crateConfig(TrioHystrix trioHystrix) {
-		ValidationBeanConfig validationBeanConfig = new ValidationBeanConfig();
-		AopStrategy<ValidationBeanConfig> aopStrategy = getBean(trioHystrix.aopStrategy(), AopStrategy.class,
-				getDefaultAopStrategy());
-		Format format = getBean(trioHystrix.format(), Format.class, getDefaultFormat());
-
-		validationBeanConfig.setAopStrategy(aopStrategy);
-		validationBeanConfig.setFormat(format);
-
-		String[] targetTimes = new String[] { ValidationBeanConfig.TIME_BEFORE };
-		validationBeanConfig.setTargetTimes(targetTimes);
-		validationBeanConfig.setExpression(new String[] { trioHystrix.HystrixModelBeanName() });
-		return validationBeanConfig;
-	}
-
-	@Around("@annotation(com.zh.cn.trio.aop.hystrix.annotation.TrioHystrix)")
-	@Override
-	public Object proxyAnnotation(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-		return super.proxyAnnotation(proceedingJoinPoint);
-	}
-}
+//package com.zh.cn.trio.aop.hystrix.aspect;
+//
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//
+//import com.zh.cn.trio.aop.hystrix.annotation.TrioHystrix;
+//
+//import io.github.zh.cn.trio.aop.croe.aspect.annotation.AbstractAnnotationConfigAspect;
+//import io.github.zh.cn.trio.aop.croe.base.format.Format;
+//import io.github.zh.cn.trio.aop.croe.strategy.AopStrategy;
+//import io.github.zh.cn.trio.aop.validation.config.ValidationBeanConfig;
+//
+//@Aspect
+//public class TrioHystrixAnnotationAspect extends AbstractAnnotationConfigAspect<ValidationBeanConfig, TrioHystrix> {
+//
+//	@Override
+//	public Class<TrioHystrix> getAnnotationClass() {
+//		return TrioHystrix.class;
+//	}
+//
+//	@SuppressWarnings("unchecked")
+//	@Override
+//	public ValidationBeanConfig crateConfig(TrioHystrix trioHystrix) {
+//		ValidationBeanConfig validationBeanConfig = new ValidationBeanConfig();
+//		AopStrategy<ValidationBeanConfig> aopStrategy = getBean(trioHystrix.aopStrategy(), AopStrategy.class,
+//				getDefaultAopStrategy());
+//		Format format = getBean(trioHystrix.format(), Format.class, getDefaultFormat());
+//
+//		validationBeanConfig.setAopStrategy(aopStrategy);
+//		validationBeanConfig.setFormat(format);
+//
+//		String[] targetTimes = new String[] { ValidationBeanConfig.TIME_BEFORE };
+//		validationBeanConfig.setTargetTimes(targetTimes);
+//		validationBeanConfig.setExpression(new String[] { trioHystrix.HystrixModelBeanName() });
+//		return validationBeanConfig;
+//	}
+//
+//	@Around("@annotation(com.zh.cn.trio.aop.hystrix.annotation.TrioHystrix)")
+//	@Override
+//	public Object proxyAnnotation(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+//		return super.proxyAnnotation(proceedingJoinPoint);
+//	}
+//}
--- aop-parent/hystrix/src/main/java/com/zh/cn/trio/aop/hystrix/validation/HystrixSpelValidataionFaceImpl.java
+++ aop-parent/hystrix/src/main/java/com/zh/cn/trio/aop/hystrix/validation/HystrixSpelValidataionFaceImpl.java
@@ -1,18 +1,18 @@
-package com.zh.cn.trio.aop.hystrix.validation;
-
-import com.zh.cn.trio.aop.hystrix.model.HystrixModel;
-
-import io.github.zh.cn.trio.aop.croe.context.AopUtilContext;
-import io.github.zh.cn.trio.aop.validation.config.ValidationBeanConfig;
-import io.github.zh.cn.trio.aop.validation.face.ValidationFace;
-
-public class HystrixSpelValidataionFaceImpl implements ValidationFace<HystrixResult> {
-
-	public HystrixResult validationData(AopUtilContext<ValidationBeanConfig> aopUtilContext) {
-		HystrixModel hystrixModel = aopUtilContext.getApplicationContext()
-				.getBean(aopUtilContext.getAopUtilConfig().getExpression()[0], HystrixModel.class);
-		return hystrixModel.hystrixValidation(aopUtilContext.getTarget(), aopUtilContext.getTargetMethod(),
-				aopUtilContext.getTargetArgs());
-	}
-
-}
+//package com.zh.cn.trio.aop.hystrix.validation;
+//
+//import com.zh.cn.trio.aop.hystrix.model.HystrixModel;
+//
+//import io.github.zh.cn.trio.aop.croe.context.AopUtilContext;
+//import io.github.zh.cn.trio.aop.validation.config.ValidationBeanConfig;
+//import io.github.zh.cn.trio.aop.validation.face.ValidationFace;
+//
+//public class HystrixSpelValidataionFaceImpl implements ValidationFace<HystrixResult> {
+//
+//	public HystrixResult validationData(AopUtilContext<ValidationBeanConfig> aopUtilContext) {
+//		HystrixModel hystrixModel = aopUtilContext.getApplicationContext()
+//				.getBean(aopUtilContext.getAopUtilConfig().getExpression()[0], HystrixModel.class);
+//		return hystrixModel.hystrixValidation(aopUtilContext.getTarget(), aopUtilContext.getTargetMethod(),
+//				aopUtilContext.getTargetArgs());
+//	}
+//
+//}
